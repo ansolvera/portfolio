715149f9544e1e2018d129cca47188c736e10684
ansolvera <38958444+ansolvera@users.noreply.github.com>
2022-12-19 14:06:10 -0800

    Add GA via Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 // https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [react(), alpinejs()],
+	integrations: [
+		react(),
+		alpinejs(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 });
